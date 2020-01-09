aa214d8cefd963776b8ce8bdad2b2852201bbd29
Ajit Joshi <ajitcj.a@gmail.com>
2020-01-09 22:24:22 +0530

    Updates

---
+++ precache-manifest.beb069e25061c4b26d9644760d7486d1.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99b72ae2852f1c35b046719cbf234870",
+    "url": "/web-sdk-staging/index.html"
+  },
+  {
+    "revision": "83427c2ea67b8850a537",
+    "url": "/web-sdk-staging/static/css/main.71f48731.chunk.css"
+  },
+  {
+    "revision": "3edbad65cb24c708114b",
+    "url": "/web-sdk-staging/static/js/2.df61ad96.chunk.js"
+  },
+  {
+    "revision": "2087c65c509bcd62d747830396457d1d",
+    "url": "/web-sdk-staging/static/js/2.df61ad96.chunk.js.LICENSE"
+  },
+  {
+    "revision": "83427c2ea67b8850a537",
+    "url": "/web-sdk-staging/static/js/main.2cd1ced7.chunk.js"
+  },
+  {
+    "revision": "a032632fd3763aabb9c6",
+    "url": "/web-sdk-staging/static/js/runtime-main.32b384b2.js"
+  },
+  {
+    "revision": "9412e1fa53766f67cd70d9c4d21a94fa",
+    "url": "/web-sdk-staging/static/media/1Layer1.9412e1fa.png"
+  },
+  {
+    "revision": "27ab9de0e41f29082e3cb26207ecca01",
+    "url": "/web-sdk-staging/static/media/2 Layer2.27ab9de0.png"
+  },
+  {
+    "revision": "c895d068ebd3c6f556651d1ea6814765",
+    "url": "/web-sdk-staging/static/media/3 Layer3.c895d068.png"
+  },
+  {
+    "revision": "87910c6a46522bfd83530411a10e1913",
+    "url": "/web-sdk-staging/static/media/4 Layer4.87910c6a.png"
+  },
+  {
+    "revision": "e293abbf8b11564b6109fd7167ebc169",
+    "url": "/web-sdk-staging/static/media/5 Mountains.e293abbf.png"
+  },
+  {
+    "revision": "35f209a3bb85343e919d705f61eec668",
+    "url": "/web-sdk-staging/static/media/6 Sun.35f209a3.png"
+  },
+  {
+    "revision": "006cdd882a154c26192c30e12b4bb642",
+    "url": "/web-sdk-staging/static/media/7 Clouds.006cdd88.png"
+  },
+  {
+    "revision": "4705f3f0084b5173c9442fc9c96a163c",
+    "url": "/web-sdk-staging/static/media/7 Clouds2.4705f3f0.png"
+  },
+  {
+    "revision": "3071d94a3d4d3c0c9dfb707cedafce3d",
+    "url": "/web-sdk-staging/static/media/8 Stars.3071d94a.png"
+  },
+  {
+    "revision": "b9d3ed89425ad6e6426474e7fdc89882",
+    "url": "/web-sdk-staging/static/media/8 Stars2.b9d3ed89.png"
+  }
+]);
